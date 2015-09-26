d10f113b6e5f6a096c079a6c0ee28bc62371c46f
Siddhant Ekale <sekale@purdue.edu>
2015-09-26 01:17:44 -0400

    added

---
+++ Parallel Programming(using pthreads)/Parallel_Programming.c
@@ -0,0 +1,14 @@
+//
+//  Parallel_Programming.c
+//  v
+//
+//  Created by Siddhant Ekale on 9/26/15.
+//  Copyright (c) 2015 Siddhant Ekale. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    printf("This program will illustrate parallel programming");
+    return(0);	
+}
