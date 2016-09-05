cf8852d64a5418dba5821f8adfe1ec24d99013fc
Siddhant <sekale@purdue.edu>
2016-09-05 02:50:35 -0400

    Problem 3 deleting a node in the middle of singly linked list

--- Linked_List/Linked_List_Project_2/main.cpp
+++ Linked_List/Linked_List_Project_2/main.cpp
@@ -5,18 +5,19 @@
 //  Created by Siddhant Ekale on 9/5/16.
 //  Copyright (c) 2016 Siddhant Ekale. All rights reserved.
 //
-//Problem 2: Algorithm to print kth to last element of a singly linked list
+//Problem 3: Algorithm to delete a node in the middle of a singly linked list given access to only that node
 
 #include <iostream>
 #include "linked_list.h"
 
 using namespace std;
-int printKthtolastelement(LinkedList list, Node *head,int k);
+
+void delete_list_node(LinkedList list, Node *delete_node);
 
 int main(int argc, const char * argv[])
 {
     Node *temp;
-    int k  = 2;
+    Node *delete_node;
     LinkedList list;
     
     temp = list.create_node(2);
@@ -28,36 +29,26 @@ int main(int argc, const char * argv[])
     temp = list.create_node(4);
     list.push(temp);
     
+    delete_node = temp; //this node needs to be deleted
+    
     temp = list.create_node(5);
     list.push(temp);
-    
+    cout << endl;
     list.print_list();
     
     //assuming we do not know the size of the linked list we find the kth to last element of the linked list
-    Node *head = list.get_head();
     
-    printKthtolastelement(list, head, k);
+    delete_list_node(list, delete_node);
+    cout << endl;
+    list.print_list();
+    
 
     return 0;
 }
 
-int printKthtolastelement(LinkedList list, Node *head, int k)
+void delete_list_node(LinkedList list, Node *delete_node)
 {
-    //terminating condition
-    if(head == NULL)
-    {
-        return 0;
-    }
-    
-    int i = printKthtolastelement(list, head -> next, k);
-    i = i + 1; //after last element this will be incremented by 1
-    
-    if(i == k)
-    {
-        cout << endl;
-        cout << head -> value;
-    }
-    
-    return i;
+    delete_node -> value = delete_node -> next -> value;
+    delete_node -> next = delete_node -> next -> next;
 }
 
