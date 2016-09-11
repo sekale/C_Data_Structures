44aa400c695f352a6bb6eb2c34064052ab6d260b
Siddhant <sekale@purdue.edu>
2016-09-11 17:12:53 -0400

    Balancing Not working yet

--- Trees/BST/main.cpp
+++ Trees/BST/main.cpp
@@ -7,12 +7,16 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
+int maxFunct(int a, int b);
+
 typedef struct Node
 {
     int value;
-    int height;
+    int height = 0;
+    int balance_factor;
     
     struct Node *left;
     struct Node *right;
@@ -28,7 +32,8 @@ TreeNode *addNode(TreeNode *root, int value)
         temp -> value = value;
         temp -> left = NULL;
         temp -> right = NULL;
-        root= temp;
+        temp -> height = 0;
+        root = temp;
     }
     
     else if(value > root -> value)
@@ -41,22 +46,85 @@ TreeNode *addNode(TreeNode *root, int value)
         root -> left = addNode(root -> left ,value);
     }
     
-    return root;
+    if(root -> left != NULL && root -> right != NULL){
+        root -> height = maxFunct(root -> left -> height, root -> right ->height) + 1;
+        root -> balance_factor = (root -> left -> height - root -> right -> height);
+    }
     
+    else if(root -> left != NULL){
+        root -> height = root -> left -> height + 1;
+        root -> balance_factor = root -> left -> height;
+    }
+    
+    else if(root -> right != NULL){
+        root -> height = root -> right -> height + 1;
+        root -> balance_factor = root -> right -> height;
+    }
+    
+    //4 conditions for balancing AVL trees
+    if(root -> balance_factor > 1)              //L
+    {
+        if(value < root -> left -> left -> value){      //LL
+            TreeNode *temp_node;
+            temp_node = root -> right;
+            root -> right = root -> right -> right;
+            root -> right -> right = temp_node;
+        }
+        
+        else{                                   //LR
+            
+        }
+    }
+    
+    else if(root -> balance_factor < -1)         //R
+    {
+        if(value < root -> left -> value)       //RL
+        {
+        }
+        
+        else{                                   //RR
+            
+        }
+    }
+    
+    //4 conditions for balancing AVL trees
+
+    
+    return root;
 }
 
-int main(int argc, const char * argv[])
+int HeightNode(TreeNode *root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+    
+    else
+    {
+        return maxFunct(HeightNode(root -> left), HeightNode(root -> right)) + 1;
+    }
+}
+
+int maxFunct(int a, int b)
+{
+    if (a > b)
+        return a;
+    
+    else
+        return b;
+}
+
+int main()
 {
     TreeNode *root = NULL;
+    int height_root = 0;
     root = addNode(root, 5);
+    root = addNode(root, 6);
     root = addNode(root, 4);
     root = addNode(root, 3);
-    root = addNode(root, 10);
     root = addNode(root, 2);
-    
-    root = addNode(root, 100);
-    root = addNode(root, 98);
-    root = addNode(root, 96);
-    root = addNode(root, 94);
+    cout << "done";
+    //cout << HeightNode(root);
 }
 
