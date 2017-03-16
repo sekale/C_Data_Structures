8a639606281af6de0fce82519b3a065f79bdfb67
Siddhant <sekale@purdue.edu>
2017-03-16 16:47:55 -0400

    Graph Representation Adj Matrix -> need to learn Adj List

---
+++ Graphs/graph.h
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class Graph
+{
+private:
+    int v;
+    int **adjMatrix;
+public:
+    Graph(int n)
+    {
+        v = n;
+        adjMatrix = new int*[n];
+        for(int i = 0; i < n; ++i)
+        {
+            adjMatrix[i] = new int[n];
+            for(int j = 0; j < n; ++j)
+                adjMatrix[i][j] = 0;
+        }
+    }
+
+    bool addEdge(int src, int dest)
+    {
+        if(src < 0 || src >= v || dest < 0 || dest >= v)
+            return false;
+
+        adjMatrix[src][dest] = 1;
+        return true;
+    }
+
+    int getNumberVertices()
+    {
+        return v;
+    }
+
+    int **getAdjMatrix()
+    {
+        return adjMatrix;
+    }
+};
